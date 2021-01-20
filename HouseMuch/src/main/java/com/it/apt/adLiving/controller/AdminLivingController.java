680bc467fe01e6ca88f525c68233a4043139ec43
subin1224 <salllena33@gmail.com>
2021-01-21 00:03:50 +0900

    20210121

---
+++ HouseMuch/src/main/java/com/it/apt/adLiving/controller/AdminLivingController.java
@@ -0,0 +1,30 @@
+package com.it.apt.adLiving.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin/adminLiving")
+public class AdminLivingController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AdminLivingController.class);
+	
+	@RequestMapping("/adLiving.do")
+	public void adLiving_get() {
+		logger.info("adLiving - 행정관리자 메인화면 보여주기");
+		//return "admin/adminLiving/adLiving";
+	}
+	
+	@RequestMapping(value = "/adNotice.do",method = RequestMethod.GET)
+	public String adNotice_get() {
+		logger.info("adNotice - 공지등록 화면보여주기");
+		return "admin/adminLiving/adNotice";
+	}
+	
+	
+	
+	
+}
---
+++ HouseMuch/src/main/java/com/it/apt/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.it.apt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+	@RequestMapping(value = "/user/mydash/userDash.do", method = RequestMethod.GET)
+	public void index() {
+		logger.info("에러가 안나라 얍!!");
+
+	}	
+	
+	@RequestMapping("/main/index.do")
+	public void index_main() {
+		logger.info("메인 화면 전체 보여주기");
+	}	
+	
+	@RequestMapping("/main/main.do")
+	public void main() {
+		logger.info("메인 화면 보여주기");
+	}
+	
+	@RequestMapping(value="/register/registerApt.do", method = RequestMethod.GET)
+	public void adminRegister_get() {
+		logger.info("APT신청하기 View!!");
+	}	
+}
